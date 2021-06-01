779c1500c37c700cfbf295a7078d8c51a8d44dd4
Philipp Schöfer <philipp.schofer@shopify.com>
2021-06-01 15:22:27 -0400

    Updates `bootstrap.sh` to run different cartridge

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 cp ~/dotfiles/.gitconfig ~/
 cp ~/dotfiles/.zshrc ~/
-cp ~/dotfiles/.shopify_config ~/.shopify_config
 
-# Only run nested steps in Spin workspaces.
 if [[ "$SPIN" = 1 ]]
 then
   # Only run nested steps in Spin + shopify/shopify workspaces.
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    cartridge insert philschoefer/some-orders-and-products
-    . /cartridges/some-orders-and-products/setup.sh
+    cd "$SPIN_REPO_SOURCE_PATH"
+    cartridge insert shameelabd/shopify-reportify-config
+    . /cartridges/shopify-reportify-config/setup.sh
+    restart
   fi
 fi
